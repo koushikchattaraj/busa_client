0ac3b2e106a0bafab95218832433463a3dd58668
Koushik Chattaraj <pri70458@adobe.com>
2025-02-20 18:15:30 +0530

    search player

--- src/component/ViewPlayers/ViewPlayers.js
+++ src/component/ViewPlayers/ViewPlayers.js
@@ -1,16 +1,20 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Loader from "../Loader/Loader";
-import { getAllPlayers } from "../../services/services";
+// import { getAllPlayers } from "../../services/services";
 import PlayerCard from "../Players/PlayerCard";
+import { allPlayerDataResponse } from "../../assets/data/allPlayerDataResponse";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 export const ViewPlayers = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [players, setPlayers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleFetch = useCallback(async () => {
     setIsLoading(true);
     try {
-      const data = await getAllPlayers();
+      // const data = await getAllPlayers();
+      const data = allPlayerDataResponse;
       const sortedData = data.data.sort((a, b) => a?.playerId - b?.playerId);
       setPlayers(sortedData);
       setIsLoading(false);
@@ -24,24 +28,38 @@ export const ViewPlayers = () => {
     handleFetch();
   }, [handleFetch]);
 
+  const filteredPlayers = players.filter((player) =>
+    player.playerName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="player-grid playerBody">
-      {isLoading && <Loader />}
-      {!isLoading && players.length !== 0 ? (
-        <>
-          {players.map((player) => (
-            // <div className="d-flex">
+    <div className="playerBody">
+      <Container fluid className="player-grid-header">
+        <Row className="justify-content-center">
+          <Col xs={12}>
+            <Form.Control
+              type="text"
+              placeholder="Search by Name"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="search-bar"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <div className="player-grid">
+        {isLoading ? (
+          <Loader />
+        ) : filteredPlayers && filteredPlayers.length > 0 ? (
+          filteredPlayers.map((player) => (
             <PlayerCard key={player._id} player={player} />
-            // </div>
-          ))}
-        </>
-      ) : (
-        !isLoading && (
+          ))
+        ) : (
           <div className="text-center mt-5">
             <h1>No Players Found</h1>
           </div>
-        )
-      )}
+        )}
+      </div>
     </div>
   );
 };
